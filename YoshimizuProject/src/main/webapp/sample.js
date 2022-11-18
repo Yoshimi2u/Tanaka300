d1a455d8e66f1ee4c11cbb1e6916062c38fb6f39
matun <matun@DESKTOP-B6H2HD2>
2022-11-18 17:00:09 +0900

    こっちでできるようにしました

---
+++ YoshimizuProject/src/main/webapp/sample.js
@@ -0,0 +1,31 @@
+$(function(){
+	$('.text').append("jqueryが読み込まれています");
+
+    $('#createbtn').on('click', function() {
+        var rgb = getScore();
+        navigator.clipboard.writeText(rgb);
+        function getScore() {
+            var score = 0;
+            var str = "";
+            var rand = 0;
+            var cnt = 0;
+            while (true) {
+                if (Number(score < 70) || Number(score > 75)) {
+                    score = 0;
+                    str = "";
+                    for (i = 0; i < 20; i++) {
+                        rand = Math.floor(Math.random() * 2) + 3
+                        str = str + rand + '\r\n';
+                        score = Number(score) + Number(rand);
+                    }
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            $("#out_dat").append(score + "点です");
+            $("#out_dat").append(cnt + "回<br>");
+            return str;
+        }
+    });
+});
