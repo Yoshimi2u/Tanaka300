103f15db50f99016d900553ce0eb982631870284
matun <matun@DESKTOP-B6H2HD2>
2022-12-11 23:35:52 +0900

    レシピを選べるようにしました。

--- YoshimizuProject/src/main/webapp/sample.js
+++ YoshimizuProject/src/main/webapp/sample.js
@@ -1,8 +1,58 @@
+var menuList = ['鶏むね肉のねぎしょうがだれ'
+  , '鳥胸肉とセロリの焼きマリネ'
+  , '鶏むねマヨクリームソース'
+  , '鶏肉のねぎま煮'
+  , '鶏肉のチリレモンバター'
+  , '豚と椎茸のゆずみそ炒め'
+  , '豚肉の梅焼き'
+  , '豚肉と青梗菜の中華炒め'
+  , '豚肉の土佐煮'
+  , 'ゆで豚のみょうがあえ'
+  , '牛肉とゴーヤのラー油炒め'
+  , '牛しそポン酢'
+  , 'つなぎなしハンバーグ'
+  , 'ドライカレー'
+  , 'ガパオ風炒め'
+  , '鮭のオリーブマスタードマリネ'
+  , 'ぶりのごま焼き'
+  , 'ぶりのしそみそ焼き'
+  , 'えびのガーリック炒め'
+  , 'たらのセロリからし蒸し'
+  , '厚揚げのピリ辛煮'
+  , '厚揚げにらポン酢'
+  , '厚揚げのツナマヨ煮'
+  , 'もやしの梅サラダ'
+  , 'もやしとにらのナムル'
+  , 'もやしとパプリカのごま酢あえ'
+  , 'ズッキーニのガーリックソテー'
+  , 'ズッキーニとなすのチーズグリル'
+  , 'ズッキーニの薬味あえ'
+  , '小松菜とトマトのニンニク炒め'
+  , '小松菜と桜えびのナムル'
+  , '小松菜のレンチン卵とじ'
+  , 'ブロッコリーのアボカド炒め'
+  , 'ブロッコリーのジンジャーサラダ'
+  , 'ブロッコリーとかぶのマスタードあえ'
+  , 'おくらおかかチーズ'
+  , 'オクラの梅のりあえ'
+  , 'オクラのわさびマリネ'
+  , 'アスパラカレーバター'
+  , 'アスパラの塩檸檬炒め'
+  , 'アスパラのからしじょうゆあえ'
+  , 'エリンギのうま煮'
+  , 'マッシュルームのエスカルゴ風'
+  , 'しいたけのゴマ塩あえ'
+  , 'まいたけのきんぴら'
+  , '親子丼'
+  , 'カレー'
+  , 'パスタ'];
 $(function() {
   $('#createbtn').on('click', function() {
-    var rgb = getScore();
-    navigator.clipboard.writeText(rgb);
+    var score = getScore().split(',');
+    navigator.clipboard.writeText(score[0]);
+    $('#showScore').html(score[1] + '点、試行回数' + score[2] + '回');
     $('#createbtn').prop('disabled', true);
+    $()
     $('#success-msg').fadeIn(1000).animate({
       'marginTop': '10px'
     }, {
@@ -18,7 +68,7 @@ $(function() {
       });
       setTimeout(function() {
         $('#createbtn').prop('disabled', false);
-      }, 400);
+      }, 600);
     }, 2300);
   });
 
@@ -46,12 +96,30 @@ $(function() {
       });
       setTimeout(function() {
         $('.btn-group').children().prop('disabled', false);
-      }, 400);
+      }, 600);
     }, 2300);
 
   });
+  $('#roulette').on('click', function() {
+    if ($('#roulette').hasClass('btn-success')) {
+      $('#roulette').removeClass('btn-success');
+      $('#roulette').addClass('btn-danger');
+      $('#roulette span').html('ストップ');
+      recipe = setInterval(function(){
+	var cnt = menuList.length +1;
+	var rand = Math.floor(Math.random() * Number(cnt));
+	$('#recipe').html(menuList[rand]);
+	},10);
+    }
+    else if ($('#roulette').hasClass('btn-danger')) {
+	  $('#roulette').removeClass('btn-danger');
+      $('#roulette').addClass('btn-success');
+      $('#roulette span').html('スタート');
+      clearInterval(recipe);
+    }
+  });
 });
-
+var max = 0;
 function getScore() {
   var score = 0;
   var str = "";
@@ -71,8 +139,7 @@ function getScore() {
       break;
     }
   }
-  $('#out_dat').children().remove();
-  $("#out_dat").append('<span>' + score + "点です" + cnt + "回</span>");
+  var str = str + ',' + score + ',' + cnt;
   $('#createbtn').blur();
   return str;
 }
@@ -103,4 +170,11 @@ function createLeftWorkDate() {
 
   var dateStr = year + "年" + month + "月" + day + "日" + hour + ":" + minute + "作業終了します。";
   return dateStr;
+}
+
+var rundomRecipe = function(){
+	var cnt = menuList.length();
+	var rand = Math.floor(Math.random() * cnt);
+	console.log(cnt + "  " + rand);
+	$('#recipe').html(menuList[cnt]);
 }
